732d5f4e92e107df03fcfd0e0444e9634d824a36
getrakuda <yuuseiyamaguchi@gmail.com>
2023-06-28 21:33:30 +0900

    first commit mayb complete.

---
+++ ChromeAndDriverKill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ChromeAndDriverKill
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var chrome = Process.GetProcessesByName("chrome");
+            var chromeDriver = Process.GetProcessesByName("chromedriver");
+
+            if (chrome.Length != 0)
+                foreach (var chromeP in chrome)
+                    chromeP.Kill();
+
+            if (chromeDriver.Length == 0)
+                foreach (var chromeDriverP in chromeDriver)
+                    chromeDriverP.Kill();
+
+            Console.WriteLine("All killed chrome process.");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ ChromeKill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ChromeKill
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var kp = Process.GetProcessesByName("chrome");
+            if (kp.Length == 0) return;
+            
+            foreach (var p in kp)
+                p.Kill();
+
+            Console.WriteLine("All killed chrome process.");
+            Thread.Sleep(3000);
+        }
+    }
+}
